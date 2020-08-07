e390f5cd53c36b9137c37f3d037be5bbe05df2ce
Irina <irinasensei@gmail.com>
2020-08-07 21:56:47 +0900

    still working on how to add POST method connecting two tables

--- client/src/App.js
+++ client/src/App.js
@@ -5,50 +5,180 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: " ",
-      users: [],
-      goals: [],
-      /*name: " ",
-      email:" ",
+      name: " ",
+      email: " ",
       goal: " ",
       deadline: " ",
       description: " ",
-      */
+      oneUserInfo: [],
     };
   }
+  // it didn't work so I went to back to my redundant update each state functions
+  updateValue = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  updateName = (e) => {
+    e.preventDefault();
+    this.setState({
+      name: e.target.value,
+    });
+  };
+
+  updateEmail = (e) => {
+    e.preventDefault();
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  updateGoal = (e) => {
+    e.preventDefault();
+    this.setState({
+      goal: e.target.value,
+    });
+  };
+
+  updateDeadline = (e) => {
+    e.preventDefault();
+    this.setState({
+      deadline: e.target.value,
+    });
+  };
+
+  updateDescription = (e) => {
+    e.preventDefault();
+    this.setState({
+      description: e.target.value,
+    });
+  };
+  addNameAndEmail() {
+    fetch("/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email,
+      }),
+    })
+      .then((res) => res.json())
+
+      .then((name) => {
+        console.log(name);
+      });
+  }
+
+  addGoalWithDeadlineAndDescription() {
+    fetch("users/goals", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        goal: this.state.goal,
+        deadline: this.state.deadline,
+        description: this.state.description,
+      }),
+    }).then((res) => res.json());
+    //.then((goal) => {
+    //console.log(this.displayUserAndGoal());
+    //});
+    //.then((goal) => {
+    //  console.log(goal);
+    //});
+  }
+
+  displayUserAndGoal() {
+    fetch("users/users_and_goals", {
+      method: "GET",
+    }).then((res) => res.json());
+  }
+  componentDidMount = async () => {
+    try {
+      const res = await fetch("/");
+      const name = await res.json();
+      const email = await res.json();
+      //this.setName({ name });
+      //this.setEmail(email);
+    } catch (error) {
+      // upon failure, show error message
+      console.log({ msg: error });
+    }
+  };
 
   render() {
+    const { name, email, goal, deadline, description } = this.state;
     return (
-      <div className="container m-5 text-center bg-light">
-        <h1>Welcome to Aimily!</h1>
-        <h3> The app that will make your dreams come true!</h3>
-        <h2>What's your name?</h2>
-        <div className="form-inline mb-4">
-          <input
-            className="form-control flex-grow-1 mr-2"
-            //value={input}
-            //onChange={this.updateInput}
-            //placeholder="Add task..."
-          />
+      <div className="container p-5 text-center bg-dark">
+        <div className="container rounded bg-light p-5">
+          <h1>Welcome to Aimily!</h1>
+          <h3> The app that will make your dreams come true!</h3>
         </div>
-        <h2>What's your email address?</h2>
-        <div className="form-inline mb-4">
-          <input className="form-control flex-grow-1 mr-2" />
+        <div className="container rounded bg-light mt-5 p-5">
+          <h2>What's your name?</h2>
+          <div className="form-inline mb-4">
+            <input
+              className="form-control flex-grow-1 mr-2"
+              value={name}
+              onChange={this.updateName}
+            />
+          </div>
+          <h2>What's your email address?</h2>
+          <div className="form-inline mb-4">
+            <input
+              className="form-control flex-grow-1 mr-2"
+              value={email}
+              onChange={this.updateEmail}
+            />
+          </div>
+
+          <button
+            className="btn btn-info btn-lg mb-4"
+            onClick={(e) => this.addNameAndEmail()}
+          >
+            Next!
+          </button>
         </div>
-        <h2>What's your goal?</h2>
-        <div className="form-inline mb-4">
-          <input className="form-control flex-grow-1 mr-2" />
+        <div className="container rounded bg-light mt-5 p-5">
+          <h2>What's your goal?</h2>
+          <div className="form-inline mb-4">
+            <input
+              className="form-control flex-grow-1 mr-2"
+              value={goal}
+              onChange={this.updateGoal}
+            />
+          </div>
+          <h2>By when do you want to achieve it?</h2>
+          <div className="form-inline mb-4">
+            <input
+              className="form-control flex-grow-1 mr-2"
+              value={deadline}
+              onChange={this.updateDeadline}
+            />
+          </div>
+          <h2>Add more details</h2>
+          <div className="form-inline mb-4">
+            <input
+              className="form-control flex-grow-1 mr-2"
+              value={description}
+              onChange={this.updateDescription}
+            />
+          </div>
+          <button
+            className="btn btn-info btn-lg mb-4"
+            onClick={(e) => this.addGoalWithDeadlineAndDescription()}
+          >
+            Let's get started!
+          </button>
         </div>
-        <h2>By when do you want to achieve it?</h2>
-        <div className="form-inline mb-4">
-          <input className="form-control flex-grow-1 mr-2" />
+        <div className="container rounded bg-light mt-5 p-5">
+          <h2>result</h2>
         </div>
-        <button
-          className="btn btn-info mb-4"
-          //onClick={(e) => this.addTask()}
-        >
-          Let's get started!
-        </button>
       </div>
     );
   }
--- routes/users.js
+++ routes/users.js
@@ -34,7 +34,7 @@ router.post("/", async (req, res, next) => {
   try {
     await db(`insert into users (name, email) values ("${name}", "${email}");`);
     // getUsers(req, res);
-    res.status(200).send("Added a new user");
+    res.status(200).send({ msg: "ok" });
   } catch (err) {
     res.status(500).send(err);
   }
@@ -48,14 +48,14 @@ router.post("/goals", async (req, res, next) => {
     await db(
       `insert into goals (goal, deadline, description) values ("${goal}", "${deadline}", "${description}");`
     );
-    //getGoals(req, res);
-    res.status(200).send("Added a new goal");
+    res.status(200).send({ msg: "ok" });
   } catch (err) {
     res.status(500).send(err);
   }
 });
 
 //++ GET users and goals temporary function to see what's inside users and goals
+/*
 const getUsersAndGoals = async (req, res, next) => {
   try {
     const results = await db(`SELECT * FROM users_and_goals ORDER BY id ASC;`);
@@ -65,8 +65,9 @@ const getUsersAndGoals = async (req, res, next) => {
   }
 };
 router.get("/users_and_goals", getUsersAndGoals);
-//POST connect user and goal in database
-//mysql> insert into users_and_goals (users_id, goal_id) VALUES (1,2);
+*/
+
+//++POST to connect user and goal in database
 
 router.post("/users_and_goals", async (req, res, next) => {
   const { users_id, goal_id } = req.body;
@@ -74,22 +75,23 @@ router.post("/users_and_goals", async (req, res, next) => {
     await db(
       `insert into users_and_goals (users_id, goal_id ) values ("${users_id}", "${goal_id}");`
     );
-    getUsersAndGoals(req, res);
+    //res.status(200).send({ msg: "ok" });
+    //getUsersAndGoals(req, res);
+    req.params.users_id = users_id;
+    getUserWithGoalById(req, res);
   } catch (err) {
     res.status(500).send(err);
   }
 });
 
-//GET all info on specific user
-//select users.name , goals.goal, goals.deadline, goals.description from users_and_goals
-//INNER JOIN users ON users_and_goals.users_id = users.id INNER JOIN goals ON users_and_goals.goal_id=goals.id;
+//++GET all info on specific user
 
 const getUserWithGoalById = async (req, res, next) => {
-  const { users_id } = req.body;
+  const { users_id } = req.params; //works with req.body as well need to check which is better for my case
 
   try {
     const results = await db(
-      `SELECT * FROM users_and_goals where id="${users_id}";`
+      `SELECT users.name , goals.goal, goals.deadline, goals.description FROM users_and_goals INNER JOIN users ON users_and_goals.users_id = users.id INNER JOIN goals ON users_and_goals.goal_id=goals.id WHERE users.id="${users_id}";`
     );
     res.send(results.data);
   } catch (err) {
