83cc08ce0c0607897fb5e6b78fed8d10ea6de757
Iva Bozic <ivabozic23@gmail.com>
2020-08-12 10:15:45 +0100

    fixed node start warning

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 const bodyParser = require("body-parser");
 const db = require("../model/helper");
-const { request } = require("../app");
 
 router.use(bodyParser.json());
 
