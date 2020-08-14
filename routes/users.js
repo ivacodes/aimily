9a1604f872c55f5f36fe0e1456c3fc28cd8a202e
Iva Bozic <ivabozic23@gmail.com>
2020-08-14 18:15:08 +0100

    removed unused routes

--- routes/users.js
+++ routes/users.js
@@ -54,7 +54,7 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
-//after user logs in, get user details, used in app.js to validate token
+//after user logs in, get user details
 router.get("/user", checkUserLoggedIn, async (req, res) => {
   console.log("yay user", req.userId, " is logged in");
   try {
@@ -65,7 +65,7 @@ router.get("/user", checkUserLoggedIn, async (req, res) => {
   }
 });
 
-//POST goals insert goal in goals table
+//POST goals - insert goal in goals table
 router.post("/goals", checkUserLoggedIn, async (req, res) => {
   const { goal, deadline, description, userId } = req.body;
   try {
@@ -79,7 +79,7 @@ router.post("/goals", checkUserLoggedIn, async (req, res) => {
   }
 });
 
-//GET goals for one user
+//GET goals - get all goals for one user id
 router.get("/goals/:userId", checkUserLoggedIn, async (req, res) => {
   const { userId } = req.params;
   try {
@@ -89,77 +89,5 @@ router.get("/goals/:userId", checkUserLoggedIn, async (req, res) => {
     res.status(404);
   }
 });
-
-//++POST create goal with deadline and description, add it to "goals" and "users_and_goals" table
-// router.post("/goals", async (req, res, next) => {
-//   const { goal, deadline, description } = req.body;
-//   try {
-//     await db(
-//       `insert into goals (goal, deadline, description) values ("${goal}", "${deadline}", "${description}");`
-//     );
-//     try {
-//       const lastUserId = await db(
-//         `SELECT ID FROM USERS ORDER BY ID DESC LIMIT 1;`
-//       );
-//       try {
-//         lastGoalId = await db(`SELECT ID FROM GOALS ORDER BY ID DESC LIMIT 1;`);
-//         try {
-//           await db(
-//             `insert into users_and_goals (users_id, goal_id ) values ("${lastUserId.data[0].ID}", "${lastGoalId.data[0].ID}");`
-//           );
-//           res.status(200).send({ msg: "ok" });
-//         } catch (err) {
-//           res.status(500).send(err);
-//         }
-//       } catch (err) {
-//         res.status(500).send(err);
-//       }
-//     } catch (err) {
-//       res.status(500).send(err);
-//     }
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// });
-
-//++ GET users and goals temporary function to see what's inside users and goals, tested but haven't used
-/*const getUsersAndGoals = async (req, res, next) => {
-  try {
-    const results = await db(`SELECT * FROM users_and_goals ORDER BY id ASC;`);
-    res.send(results.data);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-};
-router.get("/users_and_goals", getUsersAndGoals);
-*/
-
-//++POST to connect user and goal in database
-// router.post("/users_and_goals", async (req, res, next) => {
-//   const { users_id, goal_id } = req.body;
-//   try {
-//     await db(
-//       `insert into users_and_goals (users_id, goal_id ) values ("${users_id}", "${goal_id}");`
-//     );
-//     req.params.users_id = users_id;
-//     getUserWithGoalById(req, res);
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// });
-
-// ++GET all info(name, goal, deadline, description) on a specific user. Sorry, I am not sure if it is used right now in the app but it was tested and it works
-
-// const getUserWithGoalById = async (req, res, next) => {
-//   const { userId } = req.userId; //works with req.body as well need to check which is better for my case
-//   try {
-//     const results = await db(
-//       `SELECT users.name , goals.goal, goals.deadline, goals.description FROM users_and_goals INNER JOIN users ON users_and_goals.users_id = users.id INNER JOIN goals ON users_and_goals.goal_id=goals.id WHERE users.id="${users_id}";`
-//     );
-//     res.send(results.data);
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// };
 
 module.exports = router;
